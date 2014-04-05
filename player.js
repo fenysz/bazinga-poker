dc1b95325b5b91b345f53faa70e1bb711c990a1e
draven <andras.fincza@emarsys.com>
2014-04-05 10:12:26 +0200

    Toss it bro'

--- player.js
+++ player.js
@@ -1,13 +1,13 @@
-
 module.exports = {
 
-  VERSION: "Mr Bazinga version 0.1",
+    VERSION: "Mr Bazinga version 0.1",
 
-  bet_request: function(game_state) {
-    return 100;
-  },
+    bet_request: function (game_state) {
+        console.log('Do you see me?');
+        return 100;
+    },
 
-  showdown: function(game_state) {
+    showdown: function (game_state) {
 
-  }
+    }
 };
