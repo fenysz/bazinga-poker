845d5814f0a99adda3918e3c834ed445397483b5
draven <andras.fincza@emarsys.com>
2014-04-05 10:49:37 +0200

    Remove tossing

--- player.js
+++ player.js
@@ -3,11 +3,12 @@ module.exports = {
     VERSION: "Mr Bazinga version 0.1",
 
     bet_request: function (game_state) {
-	    var additional_raise = 100,
-		    us = game_state.players[game_state.in_action];
-
-	    console.log(game_state.players);
-        return (game_state.current_buy_in - us.bet + (game_state.minimum_raise + additional_raise));
+        return 500;
+//	    var additional_raise = 100,
+//		    us = game_state.players[game_state.in_action];
+//
+//	    console.log(game_state.players);
+//        return (game_state.current_buy_in - us.bet + (game_state.minimum_raise + additional_raise));
     },
 
     showdown: function (game_state) {
