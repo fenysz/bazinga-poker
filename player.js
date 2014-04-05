8c79bc383f9f010cf2edbcc994a9ebe88cbf70cc
draven <andras.fincza@emarsys.com>
2014-04-05 12:20:05 +0200

    Super AI for flop

--- player.js
+++ player.js
@@ -1,16 +1,77 @@
+var cardsRankList = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'],
+
+    VERY_GOOD = 'GOOD AS HELL',
+    GOOD = 'GOOD',
+    BAD = 'TOO BAD';
+
+var getOurPlayer = function(game_state) {
+    return game_state.players[game_state.in_action];
+};
+
+var getHand = function(player) {
+    return player.hole_cards;
+};
+
+var rateHand = function(hand) {
+    var rate = 0;
+
+    if (hand[0].rank === hand[1].rank) {
+        rate += 20;
+    }
+
+    if (hand[0].suit === hand[1].suit) {
+        rate += 10;
+    }
+
+    if (cardsRankList.indexOf(hand[0].rank) > 8) {
+        rate += 6;
+    } else if (cardsRankList.indexOf(hand[0].rank) > 5) {
+        rate += 3;
+    }
+
+    if (cardsRankList.indexOf(hand[1].rank) > 8) {
+        rate += 6;
+    } else if (cardsRankList.indexOf(hand[1].rank) > 5) {
+        rate += 3;
+    }
+
+    console.log('Rate: ', rate);
+
+    if (rate >= 20) {
+        return VERY_GOOD;
+    }
+
+    if (rate >= 12) {
+        return GOOD;
+    }
+
+    return BAD;
+};
+
+
 module.exports = {
 
-    VERSION: "Mr Bazinga version 0.1",
+    VERSION: "Mr Bazinga PreFlop Version",
 
     bet_request: function (game_state) {
-        console.log(game_state.players);
+        game_state = JSON.parse(game_state);
 
-        return 500;
-//	    var additional_raise = 100,
-//		    us = game_state.players[game_state.in_action];
-//
-//	    console.log(game_state.players);
-//        return (game_state.current_buy_in - us.bet + (game_state.minimum_raise + additional_raise));
+        var MrBazinga = getOurPlayer(game_state),
+            hand = getHand(MrBazinga),
+            rate = rateHand(hand),
+
+            minimumRaise = game_state.current_buy_in - MrBazinga.bet + game_state.minimum_raise;
+
+
+        if (rate === VERY_GOOD) {
+            return minimumRaise + 200;
+        }
+
+        if (rate === GOOD) {
+            return minimumRaise + 50;
+        }
+
+        return 0;
     },
 
     showdown: function (game_state) {
